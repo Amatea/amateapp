0b762cda2b0ab15f044686ea39e4138c0c64097b
Andrés Sarmiento <Sarmiento@amatea.org>
2016-08-16 11:46:31 -0500

    advance in the calculo controller

--- public/articles/controllers/calculoController.js
+++ public/articles/controllers/calculoController.js
@@ -1,16 +1,31 @@
 // Crear el controller 'articles'
 angular.module('articles').controller('calculoController', ['$scope', '$routeParams', '$location', 'Authentication', 'Articles',
     function($scope, $routeParams, $location, Authentication, Articles) {
-        // Exponer el service Authentication
-	$scope.article = Articles.get({
+
+  
+            // Usar el método 'get' de article para enviar una petición GET apropiada
+            $scope.article = Articles.get({
                 articleId: $routeParams.articleId
             });
-  
-  $scope.huella = Huella.query();
+        
+        
+	
+
+  // Crear un nuevo método controller para actualizar un único article
+        $scope.addArbol = function() {
+            // Usar el método '$update' de article para enviar una petición PUT apropiada
+            $scope.article.$update(function() {
+                // Si un article fue actualizado de modo correcto, redirigir el user a la página del article 
+                $location.path('/' + $scope.article._id);
+            }, function(errorResponse) {
+                // En otro caso, presenta al user un mensaje de error
+                $scope.error = errorResponse.data.message;
+            });
+        };
 
   $scope.total = function() {
           var total = [];
-          angular.forEach($scope.huella, function(item) {
+          angular.forEach($scope.article, function(item) {
               total = item.p1+item.p2+item.p3+item.p4+item.p5+item.p6+item.p7+item.p8+item.p9+item.p10+item.p11;
           })
 
@@ -58,14 +73,7 @@ angular.module('articles').controller('calculoController', ['$scope', '$routePar
 
       return total;
     }
-    $scope.addArbol = function (){
-      Huella.update($scope.dato);
-      $location.path('/');
-    };
+    
 
-    $scope.dato = Huella.get({id: $routeParams._id})
-
-  });
-
-
+    }
 ]);
