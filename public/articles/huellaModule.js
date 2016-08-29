27158174c6fcaa5a800f16cf1bd5967cd7772c0b
Andrés Sarmiento <Sarmiento@amatea.org>
2016-08-28 21:02:12 -0500

    articles services

--- public/articles/huellaModule.js
+++ public/articles/huellaModule.js
@@ -1,1 +1,13 @@
-angular.module('articles', []);
+var app = angular.module('articles', []);
+
+app.factory('Articles', ['$resource', function($resource) {
+	// Usar el service '$resource' para devolver un objeto '$resource' article
+    return $resource('api/articles/:articleId', {
+        articleId: '@_id'
+    }, {
+        update: {
+            method: 'PUT'
+        },
+        
+    });
+}]);
