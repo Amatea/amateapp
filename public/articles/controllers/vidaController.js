931c226dd189b7a75ca9e6c44344aba5f1d508b1
Andrés Sarmiento <Sarmiento@amatea.org>
2016-08-28 21:39:04 -0500

    tercer paso depurando controllers

--- public/articles/controllers/vidaController.js
+++ public/articles/controllers/vidaController.js
@@ -1,52 +1,22 @@
-// Invocar modo JavaScript 'strict'
 'use strict';
 
-// Crear el controller 'articles'
-angular.module('huellas').controller('vidaController', ['$scope', '$routeParams', '$location', 'Authentication', 'Articles',
-    function($scope, $routeParams, $location, Authentication, Articles) {
-        // Exponer el service Authentication
+angular.module('huellas').controller('vidaController', ['$scope', '$routeParams', '$location', 'Articles',
+    function($scope, $routeParams, $location, Articles) {
         
-
- // Crear un nuevo método controller para crear nuevos articles
-        $scope.create = function() {
-            // Usar los campos form para crear un nuevo objeto $resource article
-            var article = new Articles({
-                titulo: this.titulo,
-                contenido: this.contenido
-            });
-
-            // Usar el método '$save' de article para enviar una petición POST apropiada
-            article.$save(function(response) {
-                // Si un artículo fue creado de modo correcto, redireccionar al usuario a la página del artículo 
-                $location.path('articles/' + response._id);
-            }, function(errorResponse) {
-                // En otro caso, presentar al usuario el mensaje de error
-                $scope.error = errorResponse.data.message;
-            });
-        };
-
-// Crear un nuevo método controller para recuperar una lista de artículos
         $scope.find = function() {
-            // Usar el método 'query' de article para enviar una petición GET apropiada
             $scope.articles = Articles.query();
         };
 
-        // Crear un nuevo método controller para recuperar un unico artículo
         $scope.findOne = function() {
-            // Usar el método 'get' de article para enviar una petición GET apropiada
             $scope.article = Articles.get({
                 articleId: $routeParams.articleId
             });
         };
 
- // Crear un nuevo método controller para actualizar un único article
         $scope.update = function() {
-            // Usar el método '$update' de article para enviar una petición PUT apropiada
             $scope.article.$update(function() {
-                // Si un article fue actualizado de modo correcto, redirigir el user a la página del article 
                 $location.path('articles/calculo/' + $routeParams.articleId);
             }, function(errorResponse) {
-                // En otro caso, presenta al user un mensaje de error
                 $scope.error = errorResponse.data.message;
             });
         };
