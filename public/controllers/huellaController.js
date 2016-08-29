8fb932edd51a41ecb269e3d6ef0136bf04c12e0e
Andrés Sarmiento <Sarmiento@amatea.org>
2016-08-28 22:05:18 -0500

    paso quinto route controllers

--- public/controllers/huellaController.js
+++ public/controllers/huellaController.js
@@ -1,13 +1,14 @@
 'use strict';
 
-angular.module('huellas').controller('consumoController', ['$scope', '$routeParams', '$location', 'Articles',
+angular.module('huellas').controller('hogarController', ['$scope', '$routeParams', '$location', 'Articles',
     function($scope, $routeParams, $location, Articles) {
        
         $scope.find = function() {
             $scope.articles = Articles.query();
         };
-        
+
         $scope.findOne = function() {
+            
             $scope.article = Articles.get({
                 articleId: $routeParams.articleId
             });
@@ -15,11 +16,10 @@ angular.module('huellas').controller('consumoController', ['$scope', '$routePara
 
         $scope.update = function() {
             $scope.article.$update(function() {
-                
-                $location.path('articles/transporte/' + $scope.article._id);
+                $location.path('articles/consumo/' + $scope.article._id);
             }, function(errorResponse) {
                 $scope.error = errorResponse.data.message;
             });
         };
 
-}]);
+ }]);
