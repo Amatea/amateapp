28e741be0b1e9af3b82b83cebb55b32c9d1a9e92
Andres Sarmiento <sarmiento@amatea.org>
2016-10-22 07:50:49 -0500

    instalando passport google server

--- config/env/development.js
+++ config/env/development.js
@@ -2,8 +2,8 @@ module.exports = {
   db: 'mongodb://localhost/mean',
   sessionSecret: 'developmentSessionSecret',
   google: {
-    clientID: '401637428460-vsamojb5lqciiq5m1k7s4d5sh9c2gk1e.apps.googleusercontent.com',
-    clientSecret: 'N2dGszmxN5Lvmyh7MJJc8Y9K',
+    clientID: '948861021543-o130rj4k4h6u803ic9fhcmpi15bhhepi.apps.googleusercontent.com',
+    clientSecret: 'FOqDAvf2nGfbMi-trfabq8NZ',
     callbackURL: 'http://localhost:3000/oauth/google/callback'
   },
   facebook: {
