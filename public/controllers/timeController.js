23edd3be2ecfb14ec57cfa7ca87105cb37602c73
Andres Sarmiento <sarmiento@amatea.org>
2016-10-22 13:28:15 -0500

    organizando Angular Material

--- public/controllers/timeController.js
+++ public/controllers/timeController.js
@@ -5,9 +5,11 @@ var app = angular.module('huellas')
 app.controller('ExampleController', ['$scope', 'Authentication', '$window', '$routeParams', 'Articles',
   function($scope, Authentication, $window, $routeParams, Articles) {
     $scope.authentication = Authentication;
+
+    $scope.articles = Articles.query();
   }]);
 
-app.controller('consumoController', ['$scope', '$routeParams', '$location', 'Articles',
+app.controller('consum', ['$scope', '$routeParams', '$location', 'Articles',
     function($scope, $routeParams, $location, Articles) {
        
         $scope.find = function() {
